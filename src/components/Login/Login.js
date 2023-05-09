3555b1c236295524e4a0fe52528f5cf10b83ab25
johncovenant <ufotjohnamos@gmail.com>
2023-05-09 15:18:04 +0100

    dispatch

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,12 +5,15 @@ import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
 const emailReducer = () => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+  }
   return { value: '', isValid: false }
 };
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
@@ -39,7 +42,8 @@ const Login = (props) => {
 
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+    // setEnteredEmail(event.target.value);
 
     setFormIsValid(
       emailState.isValid && enteredPassword.trim().length > 6
@@ -74,7 +78,7 @@ const Login = (props) => {
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
