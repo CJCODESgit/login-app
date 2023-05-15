ee3436d8047c256016874a87cf339161beb20ec2
johncovenant <ufotjohnamos@gmail.com>
2023-05-15 14:40:40 +0100

    i've done rubbish calma

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './context/auth-context';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -31,15 +31,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <AuthContext.Provider>
-        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </AuthContext.Provider>
-    </React.Fragment>
+    <AuthContext.Provider>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider>
   );
 }
 
